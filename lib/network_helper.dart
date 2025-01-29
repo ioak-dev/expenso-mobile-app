7398df08aa3a8be52b08ba5e4906833a41390341
Mohana krishnan <mohana.krishnan@westernacher.com>
2025-01-29 11:36:41 +0530

    api changes

--- lib/network_helper.dart
+++ lib/network_helper.dart
@@ -38,7 +38,7 @@ class NetworkHelper {
         body: jsonEncode(data),
       );
       if (response.statusCode == 201 || response.statusCode == 200) {
-        return jsonDecode(response.body); 
+        return jsonDecode(response.body);
       } else {
         throw Exception('Failed to post data: ${response.statusCode}');
       }
